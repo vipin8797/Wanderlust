e1b2525ec00c4e51bcef2b7774684ace978ba137
Vipin <vy879736@gmail.com>
2026-05-27 11:48:14 +0530

    refactor(errors): improve structure of express error class and default handling

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,8 +1,17 @@
-class ExpressError extends Error{
-    constructor(status,message){
-        super();
+/**
+ * Custom operational error class for Express applications.
+ * Inherits from standard JavaScript Error to capture stack traces.
+ */
+class ExpressError extends Error {
+    /**
+     * @param {number} status - The HTTP status code (e.g. 404, 500)
+     * @param {string} message - The error explanation message
+     */
+    constructor(status, message) {
+        super(message);
         this.status = status;
-        this.message = message;
+        this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
