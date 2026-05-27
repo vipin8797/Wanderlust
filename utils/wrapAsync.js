7ede8ba5cf56d1e97b0f8e8e7d1e1f075ae9369b
Vipin <vy879736@gmail.com>
2026-05-27 11:48:23 +0530

    refactor(async): enhance wrapAsync utility function typing and comments

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,11 +1,10 @@
-// function wrapAsync(fn){
-//     return function(req,res,next){
-//         fn(req,res,next).catch((err)=>{next(err)});
-//     }
-// }
-
-// module.exports = wrapAsync;
-
+/**
+ * Asynchronous route handler wrapper that automatically catches
+ * unresolved promises and forwards errors to the next middleware.
+ * 
+ * @param {Function} fn - The asynchronous Express route/middleware handler function
+ * @returns {Function} Express middleware function
+ */
 function wrapAsync(fn) {
     return function (req, res, next) {
         Promise.resolve(fn(req, res, next)).catch(next);
